00ebb5973fe4976f3e766e7da9c392ef685a21b6
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-04-16 22:09:47 -0300

    Sistema de Oração

--- config.php
+++ config.php
@@ -2,3 +2,4 @@
 
 define("URL_BASE", "http://$_SERVER[HTTP_HOST]/www/oracoes/");
 define('ASSETS', "http://$_SERVER[HTTP_HOST]/www/oracoes/assets/");
+define("NS", 'Sistema de Oração - SDO');
--- myp/index.php
+++ myp/index.php
@@ -14,35 +14,38 @@ require_once('../protect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?=ASSETS.'css/myp.css'?>">
+    <link rel="stylesheet" href="<?=ASSETS.'css/global.css'?>">
     <title>Sistema de Orações</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand allura" style="font-size: 26px" href="#"><?=NS?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Orações</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Pastores</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown link
+                            Perfil
                         </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul class="dropdown-menu  bg-dark">
+                            <li><a class="dropdown-item text-light" href="#">Editar Perfil</a></li>
+                            <li><a class="dropdown-item text-light" href="#">Visualizar Perfil</a></li>
+                            <li class="dropdown-divider"></li>
+                            <li><a class="dropdown-item text-light" href="#">Log-out</a></li>
                         </ul>
                     </li>
                 </ul>
