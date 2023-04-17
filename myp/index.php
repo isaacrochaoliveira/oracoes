4dd4b2320ef652c60cf3772f5afa439c79514924
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-04-16 22:32:44 -0300

    Sistema de Oração

--- myp/index.php
+++ myp/index.php
@@ -52,6 +52,13 @@ require_once('../protect.php');
             </div>
         </div>
     </nav>
+    <section class="section__background-pray">
+        <div class="white">
+            <h1 class="cookie size-65">Um centro de oração para um orarmos pelos outros</h1>
+            <hr>
+            <p class="letter-spacing roboto-condensed">Vamos orar uns pelos outros, para podermos chamar a atenção de Deus</p>
+        </div>
+    </section>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
