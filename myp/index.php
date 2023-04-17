34f5f08e06137edc13be7b48f6fd1064b5633632
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-04-17 20:58:07 -0300

    SDO

--- myp/index.php
+++ myp/index.php
@@ -59,6 +59,34 @@ require_once('../protect.php');
             <p class="letter-spacing roboto-condensed">Vamos orar uns pelos outros, para podermos chamar a atenção de Deus</p>
         </div>
     </section>
+    <section class="mx-3">
+        <div class="pt-5">
+            <div class="my-3">
+                <h2 class="cookie size-50">Citações de alguns sábios</h2>
+            </div>
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
