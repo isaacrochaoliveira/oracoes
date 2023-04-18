db7a32c9fd6d0e8422924627e12e25529417ecb8
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-04-17 22:48:33 -0300

    DB

--- oracoes.sql
+++ oracoes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 17-Abr-2023 às 23:24
+-- Tempo de geração: 18-Abr-2023 às 01:46
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.2.0
 
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `citacoes`
+--
+
+DROP TABLE IF EXISTS `citacoes`;
+CREATE TABLE IF NOT EXISTS `citacoes` (
+  `id_citacao` tinyint NOT NULL AUTO_INCREMENT,
+  `nome_sabio` varchar(100) NOT NULL DEFAULT 'Desconhecido',
+  `texto_sabio` text,
+  `citacoes_curtidas` int DEFAULT '0',
+  `citacoes_comentarios` int DEFAULT '0',
+  PRIMARY KEY (`id_citacao`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `citacoes`
+--
+
+INSERT INTO `citacoes` (`id_citacao`, `nome_sabio`, `texto_sabio`, `citacoes_curtidas`, `citacoes_comentarios`) VALUES
+(1, 'Dalai Lama', '\"Só existem dois dias no ano que nada pode ser feito. Um se chama ontem e o outro se chama amanhã, portanto hoje é o dia certo para amar, acreditar, fazer e principalmente viver.\"', 0, 0),
+(2, 'Dalai Lama', '\"O período de maior ganho em conhecimento e experiência é o período mais difícil da vida de alguém.\"', 0, 0),
+(3, 'Dalai Lama', '\"Se o seu coração é absoluto e sincero, você naturalmente se sente satisfeito e confiante, não tem nenhuma razão para sentir medo dos outros.\"', 0, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `cliente`
 --
 
@@ -35,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `senha` varchar(70) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `cliente`
